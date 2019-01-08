c5484d17cc332fcd52d1952adb1d51efbbfbf0bd
李景荣(10025256) <lijr-c@glodon.com>
2019-01-08 19:19:54 +0800

    nil

--- src/js/content_script.js
+++ src/js/content_script.js
@@ -1,86 +1,154 @@
 ﻿chrome.storage.local.get(['AB_Enabled'], function (r) {
     if (r.AB_Enabled == 1) {
-        autoBuy();
+        startBuy();
     }
 })
 
-function autoBuy() {
-    chrome.storage.local.get(['AB_Start'], function (r) {
-
-        if (r.AB_Start != 1) {
-            layer.msg('Author : li_jr@hotmail.com', {
-                time: 0,
-                btn: ['开始秒杀', '关闭'],
-                btn1: function (index, layero) {
-                    chrome.storage.local.set({ AB_Start: 1 }, function () {
-                        window.location.reload();
-                    });
-                },
-                btn2: function (index, layero) {
-                    chrome.storage.local.set({ AB_Start: 0 }, function () {
-                    });
-                }
+function showSelect() {
+    layer.msg('Author : li_jr@hotmail.com', {
+        time: 0,
+        btn: ['开始秒杀', '关闭'],
+        btn1: function (index, layero) {
+            chrome.storage.local.set({ AB_Start: 1 }, function () {
+                window.location.reload();
             });
-            $('.layui-layer-dialog').css('z-index', '10000000000000');
-        }
-        else {
-            $(function () {
-                var startTime = 0,
-                    url = window.location.href;
-                var intervalId = setInterval(function () {
-                    startTime += 50;
-                    if (startTime > 500)
-                        window.location.reload();
-
-                    var btn, result = '';
-                    if (document.title.indexOf('security') != -1 || document.title.indexOf('受限') != -1) {
-                        result = '受限了，休息一会';
-                    }
-                    else if (url.indexOf('buy.tmall.com') != -1 || url.indexOf('buy.tmall.hk') != -1 || url.indexOf('buy.taobao.com') != -1 || url.indexOf('buy.m.tmall.com') != -1 || url.indexOf('h5.m.taobao.com/cart/order.html') != -1) {
-                        //提交订单页面
-                        btn = $('.go-btn');  //查找pc端提交订单button
-                        if (btn.length <= 0)
-                            btn = $('#submitOrder_1 span[title="提交订单"]'); //查找手机端提交订单button
-                        if (btn.length <= 0 && ($('#err').length > 0 || $('.warnning-text').length > 0))
-                            result = '下单过于频繁，休息一会吧';
-                    }
-                    else if (url.indexOf('.alipay.com') != -1) {
-                        //支付页面
-                        result = '下单成功';
-                    }
-                    else if (url.indexOf('.ju.taobao.com') != -1) {
-                        //聚划算
-                        result = '不支持聚划算';
-                    }
-                    else if (url.indexOf('buyertrade.taobao.com/trade/itemlist/') != -1 || url.indexOf('login.taobao.com') != -1) {
-                        //登录页面
-                        result = '未登录';
-                    }
-                    else {
-                        //购买页面
-                        btn = $('.J_LinkBuy');  //查找pc端淘宝购买button
-                        if (btn.length <= 0)
-                            btn = $('#J_LinkBuy'); //查找pc端天猫购买button
-                        if (btn.length <= 0)
-                            btn = $('.buy .btn-title'); //查找手机端淘宝购买button
-                        if (btn.length <= 0)
-                            btn = $('.trade .buy '); //查找手机端天猫购买button
-                    }
-                    if (result || btn.length > 0) {
-                        clearInterval(intervalId);
-                        if (result) {
-                            chrome.storage.local.set({ AB_Start: 0 }, function () {
-                            });
-                            layer.msg(result, {
-                                time: 1000
-                            });
-                            $('.layui-layer-dialog').css('z-index', '10000000000000');
-                            return;
-                        }
-                        btn.click();
-                    }
-                }, 50);
+        },
+        btn2: function (index, layero) {
+            chrome.storage.local.set({ AB_Start: 0 }, function () {
             });
         }
     });
+    $('.layui-layer-dialog').css('z-index', '10000000000000');
+}
+
+var intervalId;
+var isRunning;
+function startBuy() {
+    chrome.storage.local.get(['AB_Start'], function (r) {
+        console.log('auto_bug');
+        if (r.AB_Start != 1) {
+            showSelect();
+        }
+        else {
+            $(function () {
+                intervalId = setInterval(function () {
+                    if (isRunning == true) {
+                        return;
+                    }
+                    isRunning = true;
+                    chrome.storage.local.get(['AB_Enabled'], function (r) {
+                        if (r.AB_Enabled == 1) {
+                            autoBuy();
+                        } else {
+                            stopBuy();
+                            showMsg('已停止..');
+                        }
+                    })
+                }, 100);
+            });
+        }
+    });
+}
+
+function showMsg(msg) {
+    layer.msg(msg, {
+        time: 1000
+    });
+    $('.layui-layer-dialog').css('z-index', '10000000000000');
+}
+
+function stopBuy() {
+    clearInterval(intervalId);
+    chrome.storage.local.set({ AB_Start: 0 }, function () {
+    });
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function btnClick(getBtn, time, checkDisabled) {
+    time = time || 0;
+
+    if (checkDisabled) {
+        var disableBtn = $('.buy.btn-disabled ')[0] || $('.buy.disabled')[0]; //下架按钮
+        if (disableBtn) {
+            showMsg('下架中，即将刷新页面..');
+            window.location.reload();
+            return;
+        }
+    }
+
+    if (time > 500) {
+        showMsg('等待页面加载按钮超时，即将刷新页面..');
+        window.location.reload();
+        return;
+    }
+
+    var btn = getBtn();
+    if (btn) {
+        btn.click();
+        setTimeout(function () {
+            btn.click();
+        }, 100);
+        return;
+    }
+    sleep(100).then(() => {
+        time += 100;
+        btnClick(getBtn, time, checkDisabled);
+    });
+}
+
+function autoBuy() {
+    var startTime = 0, url = window.location.href;
+    startTime += 100;
+    if (startTime > 500)
+        window.location.reload();
+
+    var btn, disableBtn, result = '';
+    if (document.title.indexOf('security') != -1 || document.title.indexOf('受限') != -1) {
+        stopBuy();
+        showMsg('受限了，休息一会');
+        return;
+    }
+    if (url.indexOf('buy.tmall.com') != -1 || url.indexOf('buy.tmall.hk') != -1 || url.indexOf('buy.taobao.com') != -1 || url.indexOf('buy.m.tmall.com') != -1 || url.indexOf('h5.m.taobao.com/cart/order.html') != -1) {
+        //提交订单页面
+        btn = $('.go-btn')[0] || $('#submitOrder_1 span[title="提交订单"]')[0];  //pc端提交订单button || 手机端提交订单button
+        showMsg('提交订单中..');
+    }
+    else if (url.indexOf('.alipay.com') != -1) {
+        //支付页面
+        stopBuy();
+        showMsg('下单成功..');
+    }
+    else if (url.indexOf('.ju.taobao.com') != -1) {
+        //聚划算
+        stopBuy();
+        showMsg('不支持聚划算..');
+    }
+    else if (url.indexOf('buyertrade.taobao.com/trade/itemlist/') != -1 || url.indexOf('login.taobao.com') != -1) {
+        //登录页面
+        stopBuy();
+        showMsg('未登录..');
+    }
+    else {
+        //购买页面
+        disableBtn = $('.buy.btn-disabled ')[0] || $('.buy.disabled')[0]; //下架按钮
+        if (disableBtn) {
+            showMsg('下架中，即将刷新页面..');
+            window.location.reload();
+            return;
+        }
+        btn = $('.J_LinkBuy')[0] || $('#J_LinkBuy')[0] || $('.buy .btn-title')[0] || $('.trade .buy ')[0];  //pc端淘宝购买button || pc端天猫购买button || 手机端淘宝购买button || 手机端天猫购买button
+        showMsg('自动购买中..');
+        
+    }
+    if (btn) {
+        stopBuy();
+        btn.click();
+        setTimeout(function () {
+            btn.click();
+        }, 100);
+    }
+    isRunning = false;
 }
